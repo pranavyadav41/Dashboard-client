9e3fab6ca7c180b8b837b405da565c9ae54934ed
Pranav <akpranavyadav@gmail.com>
2024-09-09 02:37:52 +0530

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import AddEmployee from "./components/employee/AddEmployee";
-import EmployeeList from './components/employee/ListEmployees';
-import EmploymentDetails from './components/employee/EmployeeDetails';
-import EditEmployee from './components/employee/EditEmployee';
+
+// Lazy load components
+const AddEmployee = lazy(() => import('./components/employee/AddEmployee'));
+const EmployeeList = lazy(() => import('./components/employee/ListEmployees'));
+const EmploymentDetails = lazy(() => import('./components/employee/EmployeeDetails'));
+const EditEmployee = lazy(() => import('./components/employee/EditEmployee'));
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <Routes>
-          <Route path="/" element={
-            <main className="flex-grow">
-              <Hero />
-            </main>
-          } />
-          <Route path="/addEmployee" element={<AddEmployee />} />
-          <Route path="/listEmployees" element={<EmployeeList />} />
-          <Route path="/employeeDetail/:id" element={<EmploymentDetails/>} />
-          <Route path="/edit-employee/:id" element={<EditEmployee />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={
+              <main className="flex-grow">
+                <Hero />
+              </main>
+            } />
+            <Route path="/addEmployee" element={<AddEmployee />} />
+            <Route path="/listEmployees" element={<EmployeeList />} />
+            <Route path="/employeeDetail/:id" element={<EmploymentDetails />} />
+            <Route path="/edit-employee/:id" element={<EditEmployee />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
-    
-    
   );
 }
 
